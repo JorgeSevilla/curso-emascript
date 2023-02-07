a11707647bc630cf8a95e01e209cea931a25413b
Jorge Sevilla <jorge.garcia@compasso.com.br>
2023-02-07 15:34:40 -0300

    let, const, var y funciones

---
+++ src/es6/01-arrowFunction.js
@@ -0,0 +1,12 @@
+//Función normal
+function square (num){
+    return num * num;
+}
+
+//Arrow function
+const square = (num) => {
+    return num * num;
+}
+
+//Función con return implicito
+const square = num => num * num;
